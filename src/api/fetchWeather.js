b0bef7c2dcf28ceba2748e302fea2fbb6cc77b6e
musajoshua <gidijosh@gmail.com>
2026-05-30 11:57:40 +0200

    init commit

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const URL = 'https://api.weatherapi.com/v1/current.json'
+const API_KEY = '796c78b7d0734c398a275455263005'
+
+export const fetchWeather = (cityName) => {
+    return axios.get(URL, {
+        params: {
+            q: cityName,
+            key: API_KEY
+        }
+    })
+}
